33dc8df169d3529a536390025ebcac7791b37bc6
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-05-06 09:17:06 +0530

    added models.js to .

---
+++ models.js
@@ -0,0 +1,19 @@
+const { sequelize } = require("./db");
+const { DataTypes } = require("sequelize");
+sequelize.define("Task", {
+  content: {
+    type: DataTypes.STRING,
+    validate: {
+      max: 150,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    is_complete: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+});
+
+sequelize.sync();
