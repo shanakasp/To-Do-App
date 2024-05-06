1677592a729cacced4523f29b5eb3ee6dbbc1a39
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-05-06 10:08:02 +0530

    modified routes.js in .

--- routes.js
+++ routes.js
@@ -2,9 +2,26 @@ const express = require("express");
 const Task = require("./models");
 const router = express.Router();
 
-router.post("/todos", async (req, res) => {
-  const { content, description } = req.body; // Use req.body to access request body data
+// router.post("/todos", async (req, res) => {
+//   const { content, description } = req.body; // Use req.body to access request body data
 
+router.post("/todos", async (req, res) => {
+  const { content, description } = req.body;
+
+  try {
+    const newTask = await Task.create({
+      content,
+      description,
+    });
+    res.status(201).json(newTask);
+  } catch (error) {
+    console.error("Error Saving Data:", error);
+    res.status(400).json({ error: "Failed to create task" });
+  }
+});
+
+router.post("/todos", async (req, res) => {
+  const { content, description } = req.body;
   const newTask = Task.build({
     content,
     description,
@@ -12,18 +29,22 @@ router.post("/todos", async (req, res) => {
 
   try {
     await newTask.save();
-    res.status(201).json(newTask); // Respond with the newly created task
+    res.status(201).json(newTask);
   } catch (error) {
-    console.error("Error saving task:", error);
+    console.error("Error Savind Data", error);
     res.status(400).json({ error: "Failed to create task" });
   }
 });
 
-// router.post("/todos", (req, res) => {
-//   const data = request.body;
-
-//   response.status(201).json(data);
-// });
+router.get("/task", async (req, res) => {
+  try {
+    const tasks = await Task.findAll();
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error("Error fetching data");
+    res.status(400).json({ error: " Failed to load data" });
+  }
+});
 
 router.get("/todo/:id", (req, res) => {});
 
